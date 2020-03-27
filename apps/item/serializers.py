93798fa0d57a45e2e799b23ac93e327821d4615d
Sidon <sidoncd@gmail.com>
2020-03-26 22:20:22 -0300

    Postman documentation api

--- apps/item/serializers.py
+++ apps/item/serializers.py
@@ -7,7 +7,3 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ('id', 'sku', 'name', 'description', 'bar_code', 'price')
-
-    # def create(self, validated_data):
-    #     item = Item.objects.create(**validated_data)
-    #     return item
